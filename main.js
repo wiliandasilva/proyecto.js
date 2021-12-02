aec2654191f83c01f9edca8bb8941360a73a6866
wilian gonzalez da silva <wilian.da.silva97@gmail.com>
2021-12-01 23:51:23 -0300

    animacion

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 $('h1').append("<span>Zona Gamer</span>");
+
+$('span').fadeOut(2000, function () {
+    $('span').fadeIn(2000);
+  });
+
+$('.foto1').animate({ 
+    width: '70%',
+    height: '300px'},
+    "slow")
+    .delay(3000)
+    .animate({
+        width: '100%',
+        height: '450px'},
+        "slow")
+      
